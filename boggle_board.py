50c540f2bf7c34ec0f349c9afc49cb3d0e7f1e22
Shawn Kiernan <kiernan.shawn@gmail.com>
2021-06-02 13:53:27 -0500

    Shawn's boggle_board solution without step 3

--- boggle_board.py
+++ boggle_board.py
@@ -1,6 +1,43 @@
+import random
+import string
+import csv
+import os
+my_path = os.path.abspath(os.path.dirname(__file__))
+path = os.path.join(my_path, "./data/dice.csv")
+
 class BoggleBoard:
   
   def __init__(self):
-  
+    self.board = ['-'] * 16
+    self.dice = []
+
+    # import the dice
+    with open(path) as csvfile:
+      reader = csv.reader(csvfile)
+      for row in reader:
+        self.dice += row
+
+  def __str__(self):
+    output = ""
+    elem_count = 0
+    for elem in self.board:
+        if elem == 'Q':
+          output += 'Qu'+ " "
+        else:
+          output += elem + "  "
+          
+        elem_count += 1
+        if elem_count == 4:
+          output += "\n"
+          elem_count = 0
+
+    return output
 
   def shake(self):
+    for elem_index in range(len(self.board)):
+      self.board[elem_index] = random.choice(string.ascii_letters).upper()
+
+  # def shake2(self)
+myBoard = BoggleBoard()
+myBoard.shake()
+print(myBoard)
