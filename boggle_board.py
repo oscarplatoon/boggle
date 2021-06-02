17720412fd95efe7249d8ad6e2904cc60dd840b2
vzbrunstad <vzbrunstad@msn.com>
2021-06-02 16:19:07 -0500

    Vince B Boggle Release 2

--- boggle_board.py
+++ boggle_board.py
@@ -1,6 +1,39 @@
+import random
+import string
+import csv
+import os
+my_path = os.path.abspath(os.path.dirname(__file__))
+path = os.path.join(my_path, "./data/dice.csv")
+
 class BoggleBoard:
   
   def __init__(self):
-  
+    self.theboard = ['-'] * 16
+    self.thedice = ['-'] * 16
+
+  def simple_shake(self):
+    
+    for i, obj in enumerate (self.theboard):
+      self.theboard[i] = chr(random.randint(65,90))
+      # print (self.theboard)
 
   def shake(self):
+    with open(path) as csv_file:
+      csv_reader = csv.reader(csv_file)
+      for i, row in enumerate(csv_reader):
+        position = random.randint(0,15)
+        success = False
+        while success == False:
+          if self.thedice[position] == '-':
+            self.thedice[position] = (row[0][random.randint(0,5)])
+            success = True
+          else:
+            position = random.randint(0,15)
+
+
+
+        # while self.thedice[position] != '-':
+
+          print(self.thedice)
+        
+        
