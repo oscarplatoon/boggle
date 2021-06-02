6a6807454f21715e1257d5f5ac246c702b550baf
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-06-02 17:31:58 -0400

    Blank board initialization, and beginning of shake method.

--- boggle_board.py
+++ boggle_board.py
@@ -1,6 +1,25 @@
 class BoggleBoard:
-  
-  def __init__(self):
-  
+    # I'll represent the board as a list of lists [[column1],[column2], [column3], [column4]] so boggle[i][j] gives:
+    # i = row
+    # j = column
 
-  def shake(self):
+    def __init__(self):
+        self.board = self.blank_board()
+
+    def shake(self):
+        print(self.board[0][1])
+
+    #This is called at init to create a blank board.
+    def blank_board(self):
+        blank = "_"
+        output_list = []
+        for item in range(4):
+            line_on_board = []
+            for char_index in range(4):
+                line_on_board.append(blank)
+            output_list.append(line_on_board)
+        return output_list
+
+new_boggle = BoggleBoard()
+print(new_boggle.board)
+new_boggle.shake()
