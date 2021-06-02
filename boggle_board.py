18688dd3771038b031fa8e2dd4ec6d3fc8624ba0
vzbrunstad <vzbrunstad@msn.com>
2021-06-02 16:35:48 -0500

    Vince B Boggle Board solution Release 3

--- boggle_board.py
+++ boggle_board.py
@@ -22,18 +22,24 @@ class BoggleBoard:
       csv_reader = csv.reader(csv_file)
       for i, row in enumerate(csv_reader):
         position = random.randint(0,15)
+        random_letter = random.randint(0,5)
         success = False
         while success == False:
-          if self.thedice[position] == '-':
-            self.thedice[position] = (row[0][random.randint(0,5)])
+          if self.thedice[position] == '-' and row[0][random_letter] != 'Q':
+            self.thedice[position] = (f"{row[0][random_letter]}  ")
+            success = True
+          elif self.thedice[position] == '-' and row[0][random_letter] == 'Q':
+            self.thedice[position] = (f"{row[0][random_letter]}u ")
             success = True
           else:
             position = random.randint(0,15)
-
+    for i in range(0, len(self.thedice), 4):
+       #going up by 4
+       print(''.join(self.thedice [i:i + 4]))
 
 
         # while self.thedice[position] != '-':
 
-          print(self.thedice)
+          # print(self.thedice)
         
         
