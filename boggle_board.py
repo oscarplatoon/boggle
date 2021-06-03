1bc8425f9bc7a51c4077c632513a19cd1466a853
AlexanderS68 <gruntmedic68@gmail.com>
2021-06-02 22:02:23 -0400

    init solution

--- boggle_board.py
+++ boggle_board.py
@@ -1,6 +1,15 @@
 class BoggleBoard:
+  #init a boggle matrix 4 x 4
+  #with shake method print out random letters
+  #import random
+  #Each row will be 4 elements long/ call on 4 times
   
-  def __init__(self):
-  
+ 
+  def __init__(self, size):
+    self.size = 4
+    #self.board =
 
+  
+  #variable with input to shake board
   def shake(self):
+
