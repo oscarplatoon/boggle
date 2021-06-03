bd8718969718ad548aec612f73b52ded15d4c224
AlexanderS68 <gruntmedic68@gmail.com>
2021-06-02 22:11:37 -0400

    its 2200, im tired

--- boggle_board.py
+++ boggle_board.py
@@ -1,4 +1,5 @@
 import random
+import string
 
 class BoggleBoard:
   #init a boggle matrix 4 x 4
@@ -10,12 +11,16 @@ class BoggleBoard:
     self.size = 4
     #self.board =
 
-  #%s to add letters?
+  #%s to add letters? or f string
   def drawBoard(randomLetters):
     print(f'{randomLetters}, {randomLetters}, {randomLetters}, {randomLetters}')
     print(f'{randomLetters}, {randomLetters}, {randomLetters}, {randomLetters}')
     print(f'{randomLetters}, {randomLetters}, {randomLetters}, {randomLetters}')
     print(f'{randomLetters}, {randomLetters}, {randomLetters}, {randomLetters}')
+
+  #Generate random letters
+  def randomLetters(size=6, chars=string.ascii_lowercase):
+    return ''.join(random.choice(chars) for x in range(size))
 
   
   #variable with input to shake board
